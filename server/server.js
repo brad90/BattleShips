28fe77a7710dde03f57137497d71c852089a9e02
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-06-11 21:12:14 +0100

    stuff

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('client/public/index.html'))
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
