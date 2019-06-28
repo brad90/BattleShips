e297d593b3b0e895e0f8520471ce37535074c160
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-06-28 12:57:27 +0100

    where am i?

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const config = {
-  entry: `${__dirname}/client/srcapp.js`,
+  entry: `${__dirname}/client/src/app.js`,
   output: {
-    path: `${__dirname}/client/public/js`,
     filename: 'bundle.js'
+    path: `${__dirname}/client/public/js`,
   },
   mode: 'development',
 
-  
+
 };
 
 module.exports = config;
